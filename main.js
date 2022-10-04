c209bb3a82b2f54948dcc9cecb0b8d765b8aaa28
JohnnyCulbreth <johnnyculbrethjr@yahoo.com>
2022-10-03 20:28:08 -0500

    working through more todos

--- main.js
+++ main.js
@@ -19,7 +19,10 @@ class BankAccount {
         let currentBalance = this.balance();
         if (amount <= currentBalance) {
         let chargeTransaction = new Transaction(-amount, payee)
-        this.transactions.push(depositTransaction);
+        this.transactions.push(chargeTransaction);
+        console.log("It didn't work")
+        } else {
+            console.log("It didn't work")
         }
     }
 
@@ -37,4 +40,47 @@ class Transaction {
         this.payee = payee;
         this.date = new Date();
     }
+}
+
+
+
+// TESTS
+
+if (typeof describe === 'function') {
+    const assert = require('assert');
+
+describe("#Testing account creation", function() {
+    it('Should create a new account correctly', function() {
+        let acct1 = new BankAccount('xx4432', 'John Doe');
+        assert.equal(acct1.owner, 'John Doe');
+        assert.equal(acct1.accountNumber, 'xx4432');
+        assert.equal(acct1.transactions.length, 0);
+        assert.equal(acct1.balance(), 0);
+    });
+});
+
+describe("#Testing account balance", function() {
+    it('Should test account balance', function() {
+        let acct1 = new BankAccount('xx4432', 'John Doe');
+        assert.equal(acct1.balance(), 0);
+        acct1.deposit(100);
+        assert.equal(acct1.balance(), 100);
+        acct1.charge("Whole Foods", 10);
+        assert.equal(acct1.balance(), 90);
+    });
+});
+
+
+describe ("#Testing transaction creation", function() {
+    it('Should create a transaction correctly', function() {
+        let t1 = new Transaction (30, "Deposit");
+        assert.equal(t1.amount, 30);
+        assert.equal(t1.payee, "Deposit");
+    });
+    it('Should create a transaction correctly for a charge', function() {
+        let t1 = new Transaction (-49.95, "Whole Foods");
+        assert.equal(t1.amount, -49.95);
+        assert.equal(t1.payee, "Whole Foods");
+    });
+})
 }
