4cb6ee4f4a42b5a341f48dac5dc0a5f5c28c4180
JohnnyCulbreth <johnnyculbrethjr@yahoo.com>
2022-10-03 20:42:27 -0500

    finished todos and tests to confirm

--- main.js
+++ main.js
@@ -20,9 +20,7 @@ class BankAccount {
         if (amount <= currentBalance) {
         let chargeTransaction = new Transaction(-amount, payee)
         this.transactions.push(chargeTransaction);
-        console.log("It didn't work")
         } else {
-            console.log("It didn't work")
         }
     }
 
@@ -39,6 +37,19 @@ class Transaction {
         this.amount = amount;
         this.payee = payee;
         this.date = new Date();
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    constructor (accountNumber, owner, interestRate) {
+        super(accountNumber, owner);
+        this.interestRate = interestRate;
+    }
+    accrueInterest() {
+        let currentBalance = this.balance();
+        let interestAmt = currentBalance * this.interestRate;
+        let interestTransaction = new Transaction(interestAmt, "Interest");
+        this.transactions.push(interestTransaction);
     }
 }
 
@@ -68,6 +79,29 @@ describe("#Testing account balance", function() {
         acct1.charge("Whole Foods", 10);
         assert.equal(acct1.balance(), 90);
     });
+    it('should not allow a negative deposit', function(){
+        
+        const acct1 = new BankAccount('xx4432', 'James Doe');
+        assert.equal(acct1.balance(), 0);
+        acct1.deposit(100);
+        assert.equal(acct1.balance(),100);
+        acct1.deposit(-30);
+        assert.equal(acct1.balance(), 100);
+    });
+    it('should not allow charging to overdraft', function(){
+    
+        const acct1 = new BankAccount('xx4432', 'James Doe');
+        assert.equal(acct1.balance(), 0);
+        acct1.charge('target', 30);
+        assert.equal(acct1.balance(),0);
+    });
+    it('should allow a refund', function(){
+        
+        const acct1 = new BankAccount('xx4432', 'James Doe');
+        assert.equal(acct1.balance(), 0);
+        acct1.charge('target', -30);
+        assert.equal(acct1.balance(),30);
+    });
 });
 
 
@@ -82,5 +116,26 @@ describe ("#Testing transaction creation", function() {
         assert.equal(t1.amount, -49.95);
         assert.equal(t1.payee, "Whole Foods");
     });
-})
+});
+
+describe('Savings Account Creation', function(){
+    it('create account correctly', function(){
+      let saving = new SavingsAccount("xxx1234", 'Maddie Mortis', .10 );
+      assert.equal("xxx1234", saving.accountNumber);
+      assert.equal("Maddie Mortis", saving.owner);
+      assert.equal(.10, saving.interestRate);
+      assert.equal(0, saving.balance());
+    });
+
+    it('Accrue interest correctly', function(){
+      let saving = new SavingsAccount("xxx1234", 'Maddie Mortis', .10 );
+      assert.equal("xxx1234", saving.accountNumber);
+      assert.equal("Maddie Mortis", saving.owner);
+      assert.equal(.10, saving.interestRate);
+      assert.equal(0, saving.balance());
+      saving.deposit(100);
+      saving.accrueInterest();
+      assert.equal(110, saving.balance());
+    });
+}); 
 }
